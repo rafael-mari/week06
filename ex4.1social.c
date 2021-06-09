c6cd166829fdfe38528370ab7a99d30744385ce6
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-09 09:24:18 -0500

    ex4.1

---
+++ ex4.1social.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h> 
+#include <string.h>
+
+int main(void) {
+
+  printf("Your name is Rafael Marí Reyna.\n");
+  printf("Your social security number is 05-04-02-5658-8.\n");
+  printf("Your date of birth is august 31st, 2002.\n");
+
+return 0
+}
