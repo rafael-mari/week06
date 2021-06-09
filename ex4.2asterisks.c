5e1d3518ddbcf4a61757a1bb69bdda1d9e06fb52
rafael-mari <83653070+rafael-mari@users.noreply.github.com>
2021-06-09 09:30:00 -0500

    ex4.2

---
+++ ex4.2asterisks.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h> 
+#include <string.h>
+
+int main(void) {
+printf("*****\n");
+printf("*\n");
+printf("*\n");
+printf("***\n");
+printf("*\n");
+printf("*\n");
+printf("*****");
+
+}
